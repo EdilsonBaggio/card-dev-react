9796b5c3efb2a5454531309e126c3a6f2c9b61c3
Edilson <edilson.santos@kasi.com.br-at-178825947031>
2023-11-21 09:45:21 -0300

    Refactor Protfólio: Criando um portfólio em react com os dados do git

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-// import logo from './logo.svg';
 import './App.css';
-import { Repos, WhatsApp, Usergit } from './components/Scripts';
+// import { Repos, WhatsApp, Usergit } from './components/Scripts';
+import { Home } from './components/index.js';
 
 function App() {
   return (
-    <div className='container p-0'>
-        <div className="base-perfil">
-            <div>
-                <Usergit />
-            </div>
-            <WhatsApp />
-        </div>
-          <Repos />
+    // <div className='container p-0'>
+    //     <div className="base-perfil">
+    //         <div>
+    //             <Usergit />
+    //         </div>
+    //         <WhatsApp />
+    //     </div>
+    //       <Repos />
+    // </div>
+    <div className='container-fluid content-home-top'>
+      <div className='container'>
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Logo from '../logo.svg';
+
+export function Link({ href, children }) {
+    return (
+        <li>
+            <a href={href}>{children}</a>
+        </li>
+    )
+}
+
+export function Menu() {
+    return (
+        <div className="topo">
+            <img className="logo" src={Logo} alt="Minha Imagem" width='60'/>
+            <ul className="menu">
+                <Link href="/">Home</Link>
+                <Link href="/">Portofolio</Link>
+                <Link href="/">Contato</Link>
+            </ul>
+        </div>
+    );
+}
+
+export function Home() {
+    return (
+        <div>
+            <header>
+                <Menu/>
+                <ConteudoHome />
+            </header>
+        </div>
+    );
+}
+
+export function ConteudoHome() {
+    return (
+        <div className="content-home">
+            <h1>Rahadiansyah</h1>
+            <p><span>Designing</span>with purpose and precision</p>
+        </div>
+    );
+}
